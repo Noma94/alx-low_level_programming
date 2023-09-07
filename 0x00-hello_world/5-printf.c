6b8b5f267a3c519745c5ddd116234c1c6ccd3db5
Noma94 <nomamumba@gmail.com>
2023-09-07 15:29:37 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("the outcome is a piece of art with proper grammar,\n");
+		return (0);
