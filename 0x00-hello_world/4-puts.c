954c3a3415ccc506c8b8108f190f9b0dd5849240
Noma94 <nomamumba@gmail.com>
2023-09-07 13:46:24 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
- * main - A program that prints a line with puts function
- */
-int main(void)
+
+main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-Return (0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
