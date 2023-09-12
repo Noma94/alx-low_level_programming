ca1966fd508a49c2df4af993b3ed55ce23fc86d0
Noma94 <nomamumba@gmail.com>
2023-09-12 07:28:46 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet ×10 times
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+		i++;
+	}
+
+}
