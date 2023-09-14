0f5271cd13bb8ffa78a64c166700e9f6f618017c
Noma94 <nomamumba@gmail.com>
2023-09-14 10:13:29 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,10 @@
 
 /**
  * print numbers - print 0-9
+ *
+ * This function uses a for loop to iterate through the numbers from '0' to '9'
+ * and prints each number to the standard output using the _putchar function.
+ *
  * Return: void
  */
 
