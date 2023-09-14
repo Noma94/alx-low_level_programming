df27b62fa65349f4683da5574bb85a4c7e3b062c
Noma94 <nomamumba@gmail.com>
2023-09-14 12:32:42 -0700

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
  * @n: number of times the character \ should be printed
  */
 
-void print_diagonal(int n)
+void print_line(int n)
 {
 	if (n <= 0)
 	{
@@ -13,18 +14,13 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		int i, j;
+		int i;
 
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
+			_putchar('_');
 		}
+		_putchar('\n');
 	}
+
 }
