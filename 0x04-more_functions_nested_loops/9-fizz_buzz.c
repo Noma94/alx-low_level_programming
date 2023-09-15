12397dee5c113259a1f7792c2901300c6b5a210e
Noma94 <nomamumba@gmail.com>
2023-09-14 21:46:54 -0700

    Fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
+			printf("FizBuzz ");
 		}
-		else if (i % 5 == 0 && i % 3 != 0)
+		else if (i % 3 == 0)
 		{
-			printf("Buzz");
+			printf("Fizz ");
 		}
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf("FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
+			printf("Buzz ");
 		}
 		else
 		{
@@ -37,4 +33,5 @@ int main(void)
 	printf("\n");
 
 	return (0);
+
 }
