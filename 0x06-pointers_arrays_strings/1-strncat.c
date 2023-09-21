11027a07dcb803d5dfedb4e9a87d9fe434a561e4
Noma94 <nomamumba@gmail.com>
2023-09-21 00:19:57 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate two strings
+ * _strncat - concatenates two strings
  * using at most n bytes from src
  * @dest: input value
- * src: input value
+ * @src: input value
  * @n: input value
  *
  * Return: dest
