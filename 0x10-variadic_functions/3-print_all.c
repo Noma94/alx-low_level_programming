07e33790f87e11e09f11613d94fbd02c91ab6abc
Noma94 <nomamumba@gmail.com>
2023-10-12 13:46:25 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,6 +12,8 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *str, *sep = "";
 
+	va_list list;
+
 	va_start(list, format);
 
 	if (format)
@@ -21,7 +23,7 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", sep, va_arg(list, it));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
@@ -36,7 +38,7 @@ void print_all(const char * const format, ...)
 					printf("%s%s", sep, str);
 					break;
 				default:
-					i++
+					i++;
 						continue;
 			}
 			sep = ",";
