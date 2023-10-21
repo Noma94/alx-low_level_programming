43715acdfcf93468f7607d834a2aa96da4e6589c
Noma94 <nomamumba@gmail.com>
2023-10-21 10:21:46 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,10 +12,25 @@ int _strlen_recursion(char *s);
 
 int is_palindrome(char *s)
 {
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+
+int_strlen_recursion(char *s)
+{
 	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
+
 
 /**
  * check_pal - checks the characters recursively for palindrome
